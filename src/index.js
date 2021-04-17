25058d8bdb45c917f2e13bdff85d58588b1ff22a
Satyam9169 <38384042+Satyam9169@users.noreply.github.com>
2021-04-17 18:21:43 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+// Start the webapp
+const webApp = express();
+
+// Webapp settings
+webApp.use(bodyParser.urlencoded({
+    extended: true
+}));
+webApp.use(bodyParser.json());
+
+// Server Port
+const PORT = process.env.PORT;
+
+// Home route
+webApp.get('/', (req, res) => {
+    res.send(`Hello World.!`);
+});
+
+// Start the server
+webApp.listen(PORT, () => {
+    console.log(`Server is up and running at ${PORT}`);
+})
